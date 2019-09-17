38ff4d91fe662aa95c87f2d5d531c4d8a0890b61
Kevin Turcumani <kevinjaja98@gmail.com>
2019-09-17 15:15:42 +1000

    Initial commit

---
+++ Assets/Script/MouseClick.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseClick : MouseMovement
+{
+    private ParticleSystem particlesSystem;
+    public int amountOfParticle;
+
+    void Start()
+    {
+        particlesSystem = gameObject.GetComponent<ParticleSystem>();
+        particlesSystem.Stop(true);
+    }
+
+    public void LateUpdate()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            particlesSystem.Emit(amountOfParticle);
+        }
+    }
+}
